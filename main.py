8e4c254580878253a8e9dec39dfa1784af081f6a
Purpur <pemigrade@gmail.com>
2023-09-10 20:58:56 -0400

    write it on screen as secondary measure

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 # initialization stuff
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+font = pygame.font.SysFont(None, 25)
 
 while True:
     for event in pygame.event.get():
@@ -16,7 +17,10 @@ while True:
             screen.fill((0, 0, 0))
             # draw a green pixel at the click position
             pygame.draw.rect(screen, (0, 255, 0), (x, y, 1, 1))
+            text = font.render(f"x={x}, y={y}", True, (255,0,255))
+            screen.blit(text, (x + 5, y))
             # update display
             pygame.display.update()
-            # print coords
+            # print coords to chat
             print(f"x={x}, y={y}")
+            
