185e02bd07797131af52b2b830bbe099cef97acd
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-09-10 20:08:28 -0400

    Create mvp click and print coordinates

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import pyautogui
-import keyboard
+import pygame
+import sys
 
-def click_event(event):
-    if event.event_type == keyboard.KEY_DOWN and event.name == 'esc':
-        # Exit the program when the 'esc' key is pressed
-        return False
-    else:
-        # Get the current mouse coordinates and print them
-        x, y = pyautogui.position()
-        print(f"Mouse clicked at coordinates (x={x}, y={y})")
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-print("Click anywhere on the screen to get coordinates. Press 'esc' to exit.")
-keyboard.on_press_key('esc', click_event)
-keyboard.wait('esc')  # Wait for the 'esc' key to be pressed to exit the program
-keyboard.unhook_all()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            print(f"x={x}, y={y}")
