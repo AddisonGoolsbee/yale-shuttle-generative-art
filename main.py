1005ab6d3895e2811537e6dec9a6245448386c27
Purpur <pemigrade@gmail.com>
2023-09-10 20:24:58 -0400

    update gitignore remove venv

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen.fill((255, 255, 255))
 
 while True:
     for event in pygame.event.get():
@@ -11,4 +12,6 @@ while True:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
+            # need to figure out how to draw a red pixel upon click
+            #pygame.draw.rect(screen, (255, 0, 0), (x, y, 1, 1))
             print(f"x={x}, y={y}")
