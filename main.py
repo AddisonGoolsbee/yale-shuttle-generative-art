97e593edeafd607b15edf51e2e9023d403938ff9
Purpur <pemigrade@gmail.com>
2023-09-10 20:46:46 -0400

    add dots

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 import sys
 
+# initialization stuff
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen.fill((255, 255, 255))
 
 while True:
     for event in pygame.event.get():
@@ -12,6 +12,11 @@ while True:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            # need to figure out how to draw a red pixel upon click
-            #pygame.draw.rect(screen, (255, 0, 0), (x, y, 1, 1))
+            # make white background
+            screen.fill((255, 255, 255))
+            # draw a red pixel at the click position
+            pygame.draw.rect(screen, (255, 0, 0), (x, y, 1, 1))
+            # update display
+            pygame.display.update()
+            # print coords
             print(f"x={x}, y={y}")
